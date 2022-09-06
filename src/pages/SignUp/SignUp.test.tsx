ae88e0e4fedc99b587c979cd69b98fd1880eb8fd
Illia <illya1004work@gmail.com>
2022-09-06 21:55:00 +0100

    test: added interaction tests

--- src/pages/SignUp/SignUp.test.tsx
+++ src/pages/SignUp/SignUp.test.tsx
@@ -2,7 +2,6 @@ import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import SignUp from './SignUp';
 import userEvent from '@testing-library/user-event';
-import axios from 'axios';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 
@@ -33,28 +32,28 @@ describe('Sign up page', () => {
     it('Has password input', () => {
       render(<SignUp />);
       // container have same API as document
-      const passwordInput = screen.getByLabelText('Password');
+      const passwordInput = screen.getByPlaceholderText('Enter you password');
       expect(passwordInput).toBeInTheDocument();
     });
 
     it('Has password type for password input', () => {
       render(<SignUp />);
       // container have same API as document
-      const passwordInput = screen.getByLabelText('Password') as HTMLInputElement;
+      const passwordInput = screen.getByPlaceholderText('Enter you password') as HTMLInputElement;
       expect(passwordInput.type).toBe('password');
     });
 
     it('Has repeat password input', () => {
       render(<SignUp />);
       // container have same API as document
-      const passwordInput = screen.getByLabelText('Password repeat');
+      const passwordInput = screen.getByPlaceholderText('Repeat your password');
       expect(passwordInput).toBeInTheDocument();
     });
 
     it('Has password type for repeat password input', () => {
       render(<SignUp />);
       // container have same API as document
-      const passwordInput = screen.getByLabelText('Password repeat') as HTMLInputElement;
+      const passwordInput = screen.getByPlaceholderText('Enter you password') as HTMLInputElement;
       expect(passwordInput.type).toBe('password');
     });
 
@@ -72,10 +71,24 @@ describe('Sign up page', () => {
   });
 
   describe('Interactions', () => {
+    let button: HTMLButtonElement | null;
+    const setup = () => {
+      render(<SignUp />);
+      const usernameInput = screen.getByLabelText('Name') as HTMLInputElement;
+      const emailInput = screen.getByLabelText('Email') as HTMLInputElement;
+      const passwordInput = screen.getByPlaceholderText('Enter you password');
+      const repeatPasswordInput = screen.getByPlaceholderText('Repeat your password');
+      userEvent.type(usernameInput, 'username');
+      userEvent.type(emailInput, 'test@email.com');
+      userEvent.type(passwordInput, 'password');
+      userEvent.type(repeatPasswordInput, 'password');
+      button = screen.queryByRole('button', { name: 'Sign Up' });
+    }
+
     it('Enable the button when password and password repeat fields have same value', () => {
       render(<SignUp />);
-      const passwordInput = screen.getByLabelText('Password') as HTMLInputElement;
-      const repeatPasswordInput = screen.getByLabelText('Password repeat') as HTMLInputElement;
+      const passwordInput = screen.getByPlaceholderText('Enter you password') as HTMLInputElement;
+      const repeatPasswordInput = screen.getByPlaceholderText('Repeat your password');
       userEvent.type(passwordInput, 'password');
       userEvent.type(repeatPasswordInput, 'password');
       const button = screen.queryByRole('button', { name: 'Sign Up' });
@@ -92,17 +105,7 @@ describe('Sign up page', () => {
       // enable server, when user click submit,
       // msw server will intercept the real request and return status 200
       server.listen();
-      render(<SignUp />);
-      const usernameInput = screen.getByLabelText('Name') as HTMLInputElement;
-      const emailInput = screen.getByLabelText('Email') as HTMLInputElement;
-      const passwordInput = screen.getByLabelText('Password') as HTMLInputElement;
-      const repeatPasswordInput = screen.getByLabelText('Password repeat') as HTMLInputElement;
-      userEvent.type(usernameInput, 'username');
-      userEvent.type(emailInput, 'test@email.com');
-      userEvent.type(passwordInput, 'password');
-      userEvent.type(repeatPasswordInput, 'password');
-      const button = screen.queryByRole('button', { name: 'Sign Up' });
-
+      setup();
       userEvent.click(button!);
       // imitation server response with 500ms
       await new Promise((resolve) => setTimeout(resolve, 500));
@@ -111,6 +114,38 @@ describe('Sign up page', () => {
         username: 'username',
         password: 'password'
       });
+    });
+
+    it('Disables button when there is an ongoing API call', async () => {
+      // create fake server for this request
+      let counter = 0;
+      const server = setupServer(rest.post('/api/1.0/users', (req, res, ctx) => {
+        counter += 1;
+        return res(ctx.status(200));
+      }));
+      // enable server, when user click submit,
+      // msw server will intercept the real request and return status 200
+      server.listen();
+      setup();
+      userEvent.click(button!);
+      // imitation server response with 500ms
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      expect(button).toBeDisabled();
+    });
+
+    it('Displays spinner while API request in progress', async () => {
+      // create fake server for this request
+      const server = setupServer(rest.post('/api/1.0/users', (req, res, ctx) => {
+        return res(ctx.status(200));
+      }));
+      // enable server, when user click submit,
+      // msw server will intercept the real request and return status 200
+      server.listen();
+      setup();
+      // why we use queryByRole https://testing-library.com/docs/react-testing-library/cheatsheet/
+      expect(screen.queryByRole('progressbar')).not.toBeInTheDocument();
+      userEvent.click(button!);
+      expect(screen.getByRole('progressbar')).toBeInTheDocument();
     });
   });
 });
